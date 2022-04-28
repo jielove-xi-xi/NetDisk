c1bd8e96be584035436ca2e7d94bd6ddf0c4e918
Yu-Jie <92312109+jielove-xi-xi@users.noreply.github.com>
2022-04-28 18:55:48 +0800

    Update 09.双栈实现队列.cpp

--- 剑指Offer/09.双栈实现队列.cpp
+++ 剑指Offer/09.双栈实现队列.cpp
@@ -1,1 +1,39 @@
+class CQueue 
+{
+private:
+    stack<int>In_stack,Out_stack;
 
+    void in2out()
+    {
+        while(!In_stack.empty())
+        {
+            Out_stack.push(In_stack.top());
+            In_stack.pop();
+        }
+    }
+
+public:
+    CQueue()
+    {
+    }
+    
+    void appendTail(int value) 
+    {
+        In_stack.push(value);
+    }
+    
+    int deleteHead() 
+    {
+        if(Out_stack.empty())
+        {
+            if(In_stack.empty())
+            {
+                return -1;
+            }
+            in2out();
+        }
+        int value=Out_stack.top();
+        Out_stack.pop();
+        return value;
+    }
+};
